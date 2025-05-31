b8f1185ee70454c19c37b29b90db4c1fb3de0f69
franzageek <franzageek2020@gmail.com>
2025-05-31 16:09:59 +0200

    Fix leaks; fix messages

--- board.c
+++ board.c
@@ -4,11 +4,14 @@
 
 board_t* board__init(piece_t* pieces)
 {
+    printdb("initializing board...");
     u8* presence = (u8*)calloc(32, sizeof(u8));
     board_t* board = (board_t*)calloc(1, sizeof(board_t));
     if (!presence || !pieces || !board)
     {
         perror("calloc()");
+        free(presence);
+        free(board);
         return NULL;
     }
     u8 j = 1;
@@ -23,13 +26,20 @@ board_t* board__init(piece_t* pieces)
     board->state = true;
     board->indexes = presence;
     board->pieces = pieces;
+    printdb("done\n");
     return board;
 }
 
 void board__free(board_t* board)
 {
+    printdb("freeing pieces...");
     free(board->pieces);
+    printdb("done\n");
+    printdb("freeing tile array...");
     free(board->indexes);
+    printdb("done\n");
+    printdb("freeing board...");
     free(board);
+    printdb("done\n");
     return;
 }
