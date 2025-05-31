50505f846cf6a6fc54124ae5ab8376436ad40aff
franzageek <franzageek2020@gmail.com>
2025-05-31 16:12:19 +0200

    Fix messages; fix colors

--- ui.c
+++ ui.c
@@ -6,28 +6,52 @@
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
+/*SDL_Color light_tile = {255, 220, 150, 255};
+SDL_Color dark_tile = {181-60, 136-60, 99-60, 255};
+
+SDL_Color light_piece = {255, 255, 230, 200};
+SDL_Color dark_piece = {0, 0, 0, 150};*/
+
+/*SDL_Color light_piece = {248,112,96, 255};
+SDL_Color dark_piece = {179, 163, 148, 255};
+
+SDL_Color light_tile = {205, 215, 214, 255};
+SDL_Color dark_tile = {16,37,66, 255};*/
+
+SDL_Color light_piece = {244,211,94, 255};
+SDL_Color dark_piece = {218, 65, 103, 255};
+
+SDL_Color light_tile = {235, 235, 211, 255};
+SDL_Color dark_tile = {8,61,119, 255};
+
 bool ui__SDL3_init(void)
 {
+    printdb("starting SDL3 initialization...\n");
+    printdb("initializing video...");
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
         fprintf(stderr, "[SDL3 error] SDL_Init(): \'%s\'\n", SDL_GetError());
         return false;    
     }
-
+    printdb("done\n");
+    printdb("initializing window...");
     window = SDL_CreateWindow("dama v0.1", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     if (!window)
     {
         fprintf(stderr, "[SDL3 error] SDL_CreateWindow(): \'%s\'\n", SDL_GetError());
         return false;
     }
+    printdb("created window <%p> of size %ux%u\n", window, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+    printdb("initializing renderer...");
     renderer = SDL_CreateRenderer(window, NULL);
     if (!renderer)
     {
         fprintf(stderr, "[SDL3 error] SDL_CreateRenderer(): \'%s\'\n", SDL_GetError());
         return false;
     }
-
+    printdb("created renderer <%p>\n", renderer);
+    printdb("SDL3 initialization completed\n");
     return true;
 }
 
@@ -46,10 +70,10 @@ void ui__draw_board(void)
             };
 
             if ((row + col) % 2 == 0)
-                SDL_SetRenderDrawColor(renderer, 181-60, 136-60, 99-60, 255); // dark
+                SDL_SetRenderDrawColor(renderer, dark_tile.r, dark_tile.g, dark_tile.b, dark_tile.a); // dark
             
             else
-                SDL_SetRenderDrawColor(renderer, 255, 220, 150, 255); // light
+                SDL_SetRenderDrawColor(renderer, light_tile.r, light_tile.g, light_tile.b, light_tile.a); // light
 
             SDL_RenderFillRect(renderer, &rect);
         }
@@ -70,6 +94,20 @@ void render_circle(SDL_Color color, u16 cx, u16 cy, i32 radius)
 
         }
     }
+    return;
+}
+
+void draw_rect_at_xy(coord_t coord, SDL_Color color)
+{
+    SDL_FRect rect = 
+    {
+        .x = coord.x * CELL_SIZE,
+        .y = coord.y * CELL_SIZE,
+        .w = CELL_SIZE,
+        .h = CELL_SIZE
+    };
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderFillRect(renderer, &rect);
     return;
 }
 
@@ -111,9 +149,9 @@ void draw_capture_hint_circle(loc_node_t** chain, u8 count)
     
             u16 radius = CELL_SIZE / 2 - 2;
             SDL_Color color = (SDL_Color){200, 200, 100, 100};
-            draw_circle_at_xy(chain[i]->capt, radius, color);
+            draw_rect_at_xy(chain[i]->capt, color);
             radius = CELL_SIZE / 2 - 6;
-            color = (SDL_Color){0, 0, 0, 100};
+            color = (SDL_Color){255, 100, 50, 255};
             draw_circle_at_xy(chain[i]->capt, radius, color);
             radius = CELL_SIZE / 2 - 6;
             color = (SDL_Color){200, 200, 100, 100};
@@ -129,7 +167,7 @@ bool ui__draw_capture_hints(coord_t coord, board_t* board)
     {
         piece_t* piece = piece__get_from_n(coord.n, board);
         u8 count = 0;
-        loc_node_t** chain = piece__possible_captures(piece, board, NULL, &count);
+        loc_node_t** chain = piece__possible_captures(piece, board, NULL, &count, 4);
         if (chain)
         {
             draw_capture_hint_circle(chain, count);
@@ -160,36 +198,28 @@ void ui__draw_pieces(board_t* board)
                 {
                     if (piece__get_from_n(i+1, board)->player)
                     {
-                        color = (SDL_Color){255, 255, 230, 200};
-                        render_circle(color, cx, cy, radius);
+                        render_circle(light_piece, cx, cy, radius);
                     }
                     else    
                     {
-                        color = (SDL_Color){0, 0, 0, 150};
-                        render_circle(color, cx, cy, radius);
+                        render_circle(dark_piece, cx, cy, radius);
                     }
                 }
                 else
                 {
                     if (piece__get_from_n(i+1, board)->player)
                     {
-                        color = (SDL_Color){255, 255, 230, 200};
-                        render_circle(color, cx, cy, radius);
-                        color = (SDL_Color){181-60, 136-60, 99-60, 255};
+                        render_circle(light_piece, cx, cy, radius);
                         //render_circle(color, cx, cy, radius-4);
                         //color = (SDL_Color){255, 255, 230, 150};
-                        render_circle(color, cx, cy, radius-10);
-                        color = (SDL_Color){255, 255, 230, 200};
-                        render_circle(color, cx, cy, radius-20);
+                        render_circle(dark_tile, cx, cy, radius-10);
+                        render_circle(light_piece, cx, cy, radius-20);
                     }
                     else    
                     {
-                        color = (SDL_Color){0, 0, 0, 150};
-                        render_circle(color, cx, cy, radius);
-                        color = (SDL_Color){181-60, 136-60, 99-60, 255};
-                        render_circle(color, cx, cy, radius-10);
-                        color = (SDL_Color){0, 0, 0, 150};
-                        render_circle(color, cx, cy, radius-20);
+                        render_circle(dark_piece, cx, cy, radius);
+                        render_circle(dark_tile, cx, cy, radius-10);
+                        render_circle(dark_piece, cx, cy, radius-20);
                     }
                 }
             }
@@ -200,9 +230,11 @@ void ui__draw_pieces(board_t* board)
 
 void ui__quit_SDL3(void)
 {
+    printdb("freeing SDL3 resources...");
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    printdb("done\n");
     return;
 }
 
