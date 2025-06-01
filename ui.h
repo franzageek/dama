55d5e1f74da31ca5d5e2cdcbd99235c8eb8465e0
franzageek <franzageek2020@gmail.com>
2025-06-01 23:11:27 +0200

    Resize window

--- ui.h
+++ ui.h
@@ -4,8 +4,8 @@
 #include <intdef.h> 
 #include "piece.h"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 650
+#define WINDOW_HEIGHT 650
 #define BOARD_SIZE 8
 #define CELL_SIZE (WINDOW_WIDTH/BOARD_SIZE)
 
@@ -17,5 +17,7 @@ void ui__draw_hints(coord_t coord, board_t* board, bool capture_available);
 bool ui__draw_capture_hints(coord_t coord, board_t* board);
 void* ui__get_renderer(void); // use void* to prevent including SDL.h
 void* ui__get_window(void);
+
+extern int window;
 
 #endif
